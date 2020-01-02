caf52161e528ee7f06471913a4bfaa3ef413fe8f
syoukou <fatman01221087@gmail.com>
2020-01-02 23:48:15 +0900

    ListView & Database(Create)

--- app/src/main/java/com/example/android_itpmapp_homework/activities/EditActivity.java
+++ app/src/main/java/com/example/android_itpmapp_homework/activities/EditActivity.java
@@ -1,5 +1,7 @@
 package com.example.android_itpmapp_homework.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,10 +11,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.android_itpmapp_homework.R;
 
 public class EditActivity extends AppCompatActivity {
+
+    private static final String KEY_TITLE = "key_title";
+    private EditText mTitleEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,14 @@ public class EditActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.editToolbar);
         setSupportActionBar(toolbar);
 
+        mTitleEditText = findViewById(R.id.titleEditText);
+        String title = getIntent().getStringExtra(KEY_TITLE);
+        if (title != null) {
+            mTitleEditText.setText(title);
+        } else {
+            mTitleEditText.setText("");
+        }
+
         FloatingActionButton saveBtn = findViewById(R.id.saveBtn);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +42,12 @@ public class EditActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public static Intent createIntent(Context context, String title) {
+        Intent intent = new Intent(context, EditActivity.class);
+        intent.putExtra(KEY_TITLE, title);
+        return intent;
     }
 
 }
