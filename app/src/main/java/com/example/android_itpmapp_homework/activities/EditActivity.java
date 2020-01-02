1191459eee96a4cad13c8e5eea693fa636509b3f
syoukou <fatman01221087@gmail.com>
2020-01-03 00:06:28 +0900

    ListView & Database(Create & Read)

--- app/src/main/java/com/example/android_itpmapp_homework/activities/EditActivity.java
+++ app/src/main/java/com/example/android_itpmapp_homework/activities/EditActivity.java
@@ -1,7 +1,12 @@
 package com.example.android_itpmapp_homework.activities;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import com.example.android_itpmapp_homework.database.ITPMDataOpenHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,10 +14,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android_itpmapp_homework.R;
 
 public class EditActivity extends AppCompatActivity {
+
+    private static final String KEY_TITLE = "key_title";
+    private EditText mTitleEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +31,37 @@ public class EditActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.editToolbar);
         setSupportActionBar(toolbar);
 
+        mTitleEditText = findViewById(R.id.titleEditText);
+        String title = getIntent().getStringExtra(KEY_TITLE);
+        if (title != null) {
+            mTitleEditText.setText(title);
+        } else {
+            mTitleEditText.setText("");
+        }
+
         FloatingActionButton saveBtn = findViewById(R.id.saveBtn);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                String title = mTitleEditText.getText().toString();
+                if (title.isEmpty()) {
+                    Toast.makeText(EditActivity.this, getString(R.string.error_no_title), Toast.LENGTH_SHORT).show();
+                } else {
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(ITPMDataOpenHelper.COLUMN_TITLE, mTitleEditText.getText().toString());
+                    SQLiteDatabase itpmDb = new ITPMDataOpenHelper(EditActivity.this).getWritableDatabase();
+                    itpmDb.insert(ITPMDataOpenHelper.TABLE_NAME, null, contentValues);
+                    itpmDb.close();
+                    finish();
+                }
             }
         });
+    }
+
+    public static Intent createIntent(Context context, String title) {
+        Intent intent = new Intent(context, EditActivity.class);
+        intent.putExtra(KEY_TITLE, title);
+        return intent;
     }
 
 }
